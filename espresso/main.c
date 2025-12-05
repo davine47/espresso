886ea29fa3a754c6e543690ec7b6d032a8d690ce
davine47 <jackdavine47@gmail.com>
2025-12-05 10:51:05 +0800

    add debug infos 2

--- espresso/main.c
+++ espresso/main.c
@@ -1,23 +1,25 @@
 #include "espresso.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    printf("ssssssss\n");
     pPLA PLA;
 
     /* the remaining arguments are argv[optind ... argc-1] */
     PLA = NIL(PLA_t);
-    if (read_pla(stdin, &PLA) == EOF) {
+    if (read_pla(fopen(argv[1], "r"), &PLA) == EOF) {
         fprintf(stderr, "Unable to find PLA on stdin\n");
         exit(1);
     }
-
+    printf("run espresso......\n");
     /*
      *  Now run espresso
      */
+    printf("sss %ld \n", PLA->D);
     PLA->F = espresso(PLA->F, PLA->D, PLA->R);
-
+    printf("Output the solution ......\n");
     /* Output the solution */
     fprint_pla(stdout, PLA);
-
+    printf("clean up memory......\n");
     /* cleanup all used memory */
     free_PLA(PLA);
     FREE(cube.part_size);
